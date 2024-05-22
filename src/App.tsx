1b7d5be5ccaf8fb031adb7c4aa1cc8ab30487634
Lakshman Chaudhary <lakshmanchoudhary020@gmail.com>
2024-05-22 07:53:15 +0530

    feat: add tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Welcome to expense manager</h1>
+        <h1 className="text-3xl font-bold underline">Welcome to expense manager</h1>
       </div>
     </>
   )
