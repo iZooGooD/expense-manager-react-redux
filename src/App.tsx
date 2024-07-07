f8739833a0725fce42423ff63914030b30ece249
Lakshman Chaudhary <lakshmanchoudhary020@gmail.com>
2024-07-07 09:20:50 +0530

    Add types for chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-function App() {
+import Logo from "./components/common/Logo";
+
+function App(): React.JSX.Element {
   return (
     <>
       <div>
+        <Logo />
         <h1>Welcome to expense manager</h1>
       </div>
     </>
---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Logo(): JSX.Element {
+    return (
+        <Box bg='tomato'>
+            <h3>Logo here</h3>
+        </Box>
+    );
+}
